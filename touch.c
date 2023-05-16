36cff66d4a640598d65a48a044afe67137d255c6
nrocme <rochala827@gmail.com>
2023-05-15 23:04:13 -0500

    Added user option handling for touch command

--- touch.c
+++ touch.c
@@ -10,7 +10,7 @@
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <argp.h>
-
+#include <math.h>
 void printHelp();
 // This program is are recreation of touch. a linux core util that updates the edit time of a given file
 // usage ./a.out [OPTION]... FILE...
@@ -24,18 +24,17 @@ static char args_doc[] = "[FILENAME]...";
 static struct argp_option options[]= {
     {"access", 'a', 0, 0, "Change access time only"},
     {"no-create", 'c', 0, 0, "Does not create any files"},
-    {"date", 'd', "STRING", 0, "Updates access time to a given date, see GNU general date syntax"},
+    {"date", 'd', "STRING", 0, "Updates access time to a given date, example '2001-11-12 18:31:01'"},
     {"no-dereference", 'h', 0, 0, "Change only symbolic links"},
     {"file", 'f', 0, 0, "(ignored)"},
     {"modification", 'm', 0, 0, "Change modification time only"},
     {"reference", 'r', "FILE", 0, "Updates the file to the given files access and modification times"},
-    {0, 't', "STAMP", 0, "use timestamp in the format of YYYYMMDDhhmm.ss instead of current time"},
     {0}
 };
 
 struct arguments {
-    int access, modification, create, usedate, nodereference, reference, help, usetimestamp;
-    char *filename, *datestring, *referencefilename, *timestamp;
+    int access, modification, create, usedate, nodereference, usereferencefile, help;
+    char *filename, *datestring, *referencefilename;
 };
 
 /* Parse a single option. */
@@ -63,9 +62,9 @@ static error_t parse_opt (int key, char *arg, struct argp_state *state) {
         case 'h':
             arguments->nodereference = 1;
             break;
-        case 't':
-            arguments->usetimestamp = 1;
-            arguments->timestamp = arg;
+        case 'r':
+            arguments->usereferencefile = 1;
+            arguments->referencefilename = arg;
             break;
         case ARGP_KEY_ARG:
             if (state->arg_num >= 1)
@@ -89,7 +88,8 @@ static struct argp argp = { options, parse_opt, args_doc, doc };
 
 int main(int argc, char **argv) {
     struct arguments arguments;
-    int fd;
+    struct stat buf;
+    int fd, rfd;
     char *arg;
     struct timespec times[2];
     /* Default values. */
@@ -99,24 +99,50 @@ int main(int argc, char **argv) {
     arguments.usedate = 0;
     arguments.datestring = NULL;
     arguments.nodereference = 0;
-    arguments.reference = 0;
-    arguments.usetimestamp = 0;
-    arguments.timestamp = NULL;
+    arguments.usereferencefile = 0;
     arguments.referencefilename = NULL;
     arguments.filename = NULL;
-    times[0].tv_nsec = UTIME_NOW;
-    times[1].tv_nsec = UTIME_NOW;
+    times[0].tv_nsec = 0;
+    times[1].tv_nsec = 0;
+    times[0].tv_sec = 0;
+    times[1].tv_sec = 0;
     /* Parse our arguments; every option seen by parse_opt will
         be reflected in arguments. */
     argp_parse (&argp, argc, argv, 0, 0, &arguments);
 
+    // Checking user options to decide how to update the files access and modification times.
+    if(arguments.usedate){
+        struct tm date;
+        if(strptime(arguments.datestring, "%Y-%m-%d %H:%M:%S", &date) != NULL){
+            time_t epoch = mktime(&date);
+            times[0].tv_sec = epoch;
+            times[1].tv_sec = epoch;
+        } else {
+            printf("Invalid time stamp given not file times were changed and no files were created\n");
+            return(1);
+        }
+    } else if(arguments.usereferencefile){
+        // open reference file descriptor
+        rfd = openat(AT_FDCWD, arguments.referencefilename, O_WRONLY|O_LARGEFILE|O_NONBLOCK, 0666);
+        // grab atime and mtime from fstat
+        fstat(rfd, &buf);
+        printf("%d\n", buf.st_atime);
+        times[0].tv_sec = buf.st_atime;
+        times[1].tv_sec = buf.st_mtime;
+    } else {
+        times[0].tv_nsec = UTIME_NOW;
+        times[1].tv_nsec = UTIME_NOW;
+    }
+
+    // Opening file, to be changed, passed by the user. Checking the user flag for no-create. Creating the file if it does no exist and the user does not specify no-create option.
     if(arguments.create) fd = openat(AT_FDCWD, arguments.filename, O_WRONLY|O_CREAT|O_LARGEFILE|O_NONBLOCK, 0666);
     else fd = openat(AT_FDCWD, arguments.filename, O_WRONLY|O_LARGEFILE|O_NONBLOCK, 0666);
 
 
+    // Checking access and modification flags set by user options.
+    // UTIME_OMIT represents a special value that tells the system call futimes to not change the time that is currently set.
     if(!arguments.access) times[0].tv_nsec = UTIME_OMIT;
     if(!arguments.modification) times[1].tv_nsec = UTIME_OMIT;
-
 
 
     if(futimens(fd, times)== -1) {
@@ -125,48 +151,6 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
-/*
-    //Init variables
-    int fd;
-    char *option, *filepath;
-    time_t currtime;
-    struct tm *timeinfo;
-
-    // Getting current localtime
-    currtime = time(0);
-    timeinfo = localtime (&currtime);
-    //printf("Current system time: %s", asctime(timeinfo));
-    if(argc == 1)
-    for(int i = 1; i < argc; i++) printf("Try 'touch --help' for more information\n");
-    if(argc == 3){
-        // do nothing
-    } else if(argc == 2) {
-        option = argv[1];
-        //check to make sure not asking for help page and that usage is valid
-        if(strcmp(option, "--help") == 0) {
-            printHelp();
-        }
-        else if(argv[1][0] == '-') {
-            printf("Invalid use of option or unknown option.\n");
-            printHelp();
-        }
-        else {
-            // standard touch usage.
-            // update file access and modification time.
-            // if file does exist create the file
-            filepath = argv[1];
-            fd = open(filepath, O_CREAT);
-            if(futimes(fd, NULL)== -1) {
-                perror("futimens");
-                printf("Errno: %d\n", errno);
-                exit(EXIT_FAILURE);
-            }
-        }
-
-    } else {
-        // incorrect usage
-        printf("Try 'touch --help' for more information\n");
-    }*/
     return(0);
 }
 
